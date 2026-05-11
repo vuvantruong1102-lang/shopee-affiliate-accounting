7cd3a21c47ef8f5a08d246eb81d7eb6491f29174
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-05-12 06:54:51 +0700

    Update bank-book-view.tsx

--- components/books/bank-book-view.tsx
+++ components/books/bank-book-view.tsx
@@ -232,17 +232,6 @@ export function BankBookView({
         />
       </div>
 
-      {/* Chart */}
-      {dailyData.length > 0 && (
-        <Card className="print:hidden">
-          <CardHeader>
-            <CardTitle className="text-base">Biến động theo ngày</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <DailyChart data={dailyData} />
-          </CardContent>
-        </Card>
-      )}
 
       {/* Transactions table */}
       <Card>
